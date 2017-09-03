d83449e61a9db1488ab8e0a15803142d6b53e046
samid737 <dara_s@live.nl>
2017-09-03 16:59:57 +0200

    ellipse&circle interset

--- phaser-ce/main.js
+++ phaser-ce/main.js
@@ -29,9 +29,8 @@ function create() {
 }
 
 function update() {
-  intersectionsEllipse=intersectsLine(ellipse,line1,true);
-  intersectionsCircle=intersectsLine(circle,line1,true);
-  
+  intersectionsEllipse=Phaser.Ellipse.intersectsLine(ellipse,line1,true);
+  intersectionsCircle=Phaser.Circle.intersectsLine(circle,line1,true);  
   line1.fromSprite(handle1, handle2, false);
 }
 
@@ -43,47 +42,6 @@ function render() {
   intersectionsEllipse.forEach(function(point){game.debug.geom(point,'rgba(255,0,255,1)')});  
   intersectionsCircle.forEach(function(point){game.debug.geom(point,'rgba(255,0,255,1)')});  
   
-  game.debug.text("intersects circle: "+intersectsLine(circle,line1),32,32);
-  game.debug.text("intersects ellipse: "+intersectsLine(ellipse,line1),32,64);
-  
-}
-
-
-
-
-
-function intersectsLine(ellipse,line,returnpoints){
-  // ellipse: ((x - h)/(a*a))^2 + ((y - k)/(b*b))^2 = 1
-  // line: y = m * x + n
-  // h: x value of circle centre
-  // k: y value of circle centre
-  // m: slope
-  // n: y-intercept
-  var h=ellipse.x;
-  var k=ellipse.y;
-  var m=((line.end.y-line.start.y)/(line.end.x-line.start.x));
-  var n= line.end.y- (m*line.end.x);
-  
-  if(ellipse instanceof Phaser.Ellipse){
-    var a= ellipse.width/2;
-    var b= ellipse.height/2;
-  }else{
-    var a=ellipse.radius;
-    var b=ellipse.radius;
-  }
-  var del= n+m*h;
-
-  var x0=( h*(b*b)- m*(a*a) * (n-k) +  a*b* (Math.sqrt((a*a)*(m*m)+(b*b)-(del*del)-(k*k) + (2*del*k))))/((a*a)*(m*m)+(b*b));
-  var x1=( h*(b*b)- m*(a*a) * (n-k) -  a*b* (Math.sqrt((a*a)*(m*m)+(b*b)-(del*del)-(k*k) + (2*del*k))))/((a*a)*(m*m)+(b*b));
-    
-  var y0= m*x0 + n;
-  var y1=m*x1 +n;
-  var p0= new Phaser.Point(x0,y0);
-  var p1= new Phaser.Point(x1,y1);
-  
-  if(line.pointOnSegment(p0.x,p0.y,0.01)||line.pointOnSegment(p1.x,p1.y,0)){
-      return returnpoints?[p0,p1]:true;          
-    }else{
-      return returnpoints?[]:false;
-    }
+  game.debug.text("intersects circle: "+Phaser.Circle.intersectsLine(circle,line1),32,32);
+  game.debug.text("intersects ellipse: "+Phaser.Ellipse.intersectsLine(ellipse,line1),32,64);
 }
